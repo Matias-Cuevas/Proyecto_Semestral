af06d2b03fe33c8c848dd8399b5ccb097461d8a2
Matias Cuevas <mat.cuebas@duocuc.cl>
2022-06-01 01:05:21 -0400

    formularios_perfil

--- JS/validacion.js
+++ JS/validacion.js
@@ -174,6 +174,123 @@ $("#form2").submit(function (e) {
 });
 
 
+$("#form_perf").submit(function (e) {
+  e.preventDefault();
+  let mensajesMostrar = "";
+  let entrar = false;
+  var contra1 = $("#con3").val();
+  var contra2 = $("#con4").val();
+  var nombre = $("#nom2").val();
+  var letras = "ABCDEFGHIJKMNÑLOPQRSTUVWXYZ";
+          
+
+
+
+  if (nombre.length < 4 || nombre.length >10) {
+      mensajesMostrar += "El nombre debe tener entre 4 y 10 caracteres. \n ";
+      entrar = true;
+  }
+  var letraInicial = nombre.charAt(0);
+
+  if (!esMayus(letraInicial)) {
+      mensajesMostrar += "La primera letra del nombre debe ser mayúscula.\n";
+      entrar = true;
+  }
+  
+
+  function tiene_mayus(texto){
+      for(i=0; i<texto.length; i++){
+     if (letras.indexOf(texto.charAt(i),0)!=-1){
+      return 0;
+                                                  }
+                              }
+       return 1;
+  }
+
+  
+
+   function correlativo(texto){
+      for(i=0; i<texto.length; i++){
+         if (Number(texto.charAt(i))+1==Number(texto.charAt(i+1))){
+             return 0;
+             
+         }
+     
+      }
+  }
+
+ 
+
+  if ((/\d/.test(contra1) && isNaN(contra1))==false) {
+    mensajesMostrar += "La contraseña debe tener al menos 1 número y 1 letra.\n";
+    entrar = true;
+}
+if (contra1.length < 8 || contra1.length >18) {
+    mensajesMostrar += "La contraseña debe tener entre 8 y 18 caracteres \n ";
+    entrar = true;
+}
+
+
+
+
+
+if(tiene_mayus(contra1)){
+  mensajesMostrar += "La contraseña debe tener al menos una mayúscula\n";
+  entrar = true;
+}
+
+if (/\s/.test(contra1)){
+  mensajesMostrar += "La contraseña no puede tener espacios en blanco \n ";
+  entrar = true;
+}
+
+function correlativo(texto){
+  for(i=0; i<texto.length; i++){
+     if (Number(texto.charAt(i))+1==Number(texto.charAt(i+1))){
+         return 0;
+         
+     }
+ 
+  }
+}
+
+if (correlativo(contra1)==0){
+  mensajesMostrar += "La contraseña no puede tener 2 numero correlativos \n ";
+  entrar = true;
+  }
+
+
+  if (contra1!=contra2){
+    mensajesMostrar += "Las contraseñas no coinciden \n ";
+   entrar = true;   
+}
+
+  if (entrar) {
+       swal({
+              title: "Error",
+              text: (mensajesMostrar),
+              type: "error"
+          });
+    
+      
+  }
+  else {
+          swal({
+              title: "Cambios confirmados",
+              text: "",
+              confirmButtonText: "OK",
+              type: "success"
+          }, function() {
+              window.location ="principal.html";})
+      
+      
+  }
+});
+
+
+
+
+
     
 });
 
